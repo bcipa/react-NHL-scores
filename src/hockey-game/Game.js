2c705408b507df4ba27f718e9b28b2ed4e5aff35
bcipa <39407286+bcipa@users.noreply.github.com>
2023-01-22 22:21:10 -0500

    Worked on dynamic info, devils logo

--- src/hockey-game/Game.js
+++ src/hockey-game/Game.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 
-import TeamInfo from './TeamInfo';
+function Team(props) {
+  return (
+    <div className="team-info">
+      <img
+        className="team-logo"
+        src={`/assets/imgs/logos/${props.name.toLowerCase()}.svg`}
+      />
+      <span>{props.name}</span>
+    </div>
+  );
+}
+
+function TeamInfo(props) {
+  const isOpponent = props.isOpponent;
+  return (
+    <div className="team-score">
+      {isOpponent ? <h1>1</h1> : null}
+      <Team name={props.name} />
+      {!isOpponent ? <h1>3</h1> : null}
+    </div>
+  );
+}
 
 function GameInfo(props) {
   return (
     <div className="game-info">
-      <p className="game-info-detail">PPG Paints Arena</p>
-      <p className="game-info-detail">January 2, 2023</p>
+      <p className="game-info-detail">{props.arena}</p>
+      <p className="game-info-detail">{props.date}</p>
     </div>
   );
 }
@@ -14,13 +35,48 @@ function GameInfo(props) {
 export default class Game extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      selectedTeamName: 'Penguins',
+      selectedTeamId: 5,
+      selectedTeamScore: 0,
+      opponent: 'Penguins',
+      opponentScore: 0,
+      arena: '',
+      date: '',
+    };
   }
 
   importGameData(data) {
-    console.log(data);
+    const awayTeam = data['teams']['away'];
+    const homeTeam = data['teams']['home'];
+    let selectedTeam = null;
+    let opponent = null;
+
+    if (this.state.selectedTeamId == awayTeam['team']['id']) {
+      selectedTeam = awayTeam;
+      opponent = homeTeam;
+    } else {
+      selectedTeam = homeTeam;
+      opponent = awayTeam;
+    }
+
+    let opponentName = opponent['team']['name'].split(' ');
+    let opponentScore = opponent['teamStats']['teamSkaterStats']['goals'];
+    let selectedTeamName = selectedTeam['team']['name'].split(' ');
+    let selectedTeamScore =
+      selectedTeam['teamStats']['teamSkaterStats']['goals'];
+
+    this.setState({
+      opponent: opponentName[opponentName.length - 1],
+      opponentScore: opponentScore,
+      selectedTeamName: selectedTeamName[selectedTeamName.length - 1],
+      selectedTeamScore: selectedTeamScore,
+    });
   }
 
   importTeamData(data) {
+    console.log(data);
     var gamePath =
       data['teams'][0]['previousGameSchedule']['dates'][0]['games'][0][
         'content'
@@ -43,11 +99,18 @@ export default class Game extends Component {
   render() {
     return (
       <div className="game-container">
-        <TeamInfo name="Penguins" />
+        <TeamInfo
+          name={this.state.selectedTeamName}
+          score={this.state.selectedTeamScore}
+        />
 
-        <GameInfo />
+        <GameInfo arena={this.state.arena} date={this.state.date} />
 
-        <TeamInfo name="Penguins" isOpponent={true} />
+        <TeamInfo
+          name={this.state.opponent}
+          score={this.state.opponentScore}
+          isOpponent={true}
+        />
       </div>
     );
   }
--- src/hockey-game/TeamInfo.js
+++ src/hockey-game/TeamInfo.js
@@ -5,30 +5,30 @@ function Team(props) {
     <div className="team-info">
       <img
         className="team-logo"
-        src={`/assets/imgs/logos/${props.teamName.toLowerCase()}.svg`}
+        src={`/assets/imgs/logos/${props.name.toLowerCase()}.svg`}
       />
-      <span>{props.teamName}</span>
+      <span>{props.name}</span>
     </div>
   );
 }
 
+function TeamInfo(props) {
+  const isOpponent = props.isOpponent;
+  return (
+    <div className="team-score">
+      {isOpponent ? <h1>1</h1> : null}
+      <Team name={props.name} />
+      {!isOpponent ? <h1>3</h1> : null}
+    </div>
+  );
+}
 export default class TeamInfo extends Component {
   constructor(props) {
     super(props);
     console.log(props);
-    this.state = {
-      teamName: props.name,
-      isOpponent: props.isOpponent,
-    };
   }
 
   render() {
-    return (
-      <div className="team-score">
-        {this.state.isOpponent ? <h1>1</h1> : null}
-        <Team teamName={this.state.teamName} />
-        {!this.state.isOpponent ? <h1>3</h1> : null}
-      </div>
-    );
+    return <div>hi</div>;
   }
 }
